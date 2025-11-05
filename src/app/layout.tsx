6f03d599e7e582c0078472838df9a85d3d93058a
Zachary <16055354+zachary_dev@user.noreply.gitee.com>
2025-11-05 17:26:30 +0800

    feat: Add Google Tag Manager and fix linting issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -112,8 +113,26 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KVW4H6PX"
+            height="0"
+            width="0"
+            className="gtm-noscript-iframe"
+            title="Google Tag Manager"
+          ></iframe>
+        </noscript>
         {children}
       </body>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KVW4H6PX');
+        `}
+      </Script>
     </html>
   );
 }
